c6d9be1305fbe50d0a98349deb41678ef445d1a8
Azones <59911700+Azonesbz@users.noreply.github.com>
2023-09-15 11:10:47 +0200

    added reducer to application

---
+++ src/reducer/index.js
@@ -0,0 +1,23 @@
+
+export const initialState = {
+    total: 0,
+    expense: [],
+    lastAction: ''
+}
+
+
+const calcReducer = (state, action) => {
+    switch(action.type) {
+
+        case 'add':
+           
+        case "delete":
+           
+        case 'total':
+          
+        default:
+            return state;
+    }
+}
+
+export default calcReducer
