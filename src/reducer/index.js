f6c5ef0aaa96ca826850ab317297222757156f67
Azones <59911700+Azonesbz@users.noreply.github.com>
2023-09-15 17:28:14 +0200

    finished exercices and changed style of application

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,66 +1,62 @@
-
 export const TAG = {
-    PRICE: 'price',
-    CATEGORY: 'category',
-    ALL: 'all'
-}
+  PRICE: "price",
+  CATEGORY: "category",
+  ALL: "all",
+};
 
 export const ERROR_TYPES = {
-    EMPTY_INPUT: 'empty_error',
-    INVALID_TYPE: 'invalid_type'
-}
-
+  EMPTY_INPUT: "empty_error",
+  INVALID_TYPE: "invalid_type",
+};
 
 export const initialState = {
-    total: 0,
-    expense: [],
-    inputError: {
-        tag: '',
-        msg: '',
-        type: ''
-    }
-}
+  total: 0,
+  expense: [],
+  inputError: {
+    tag: "",
+    msg: "",
+    type: "",
+  },
+};
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'error':
-            return {
-                ...state,
-                inputError: {
-                    tag: action.payload.tag,
-                    msg: action.payload.msg,
-                    type: action.payload.type,
-                }
-            }
+  switch (action.type) {
+    case "error":
+      return {
+        ...state,
+        inputError: {
+          tag: action.payload.tag,
+          msg: action.payload.msg,
+          type: action.payload.type,
+        },
+      };
 
-        case 'add':
-            console.log(action.payload)
-            return {
-                ...state,
-                expense: [
-                    ...state.expense,
-                    {
-                        price: parseInt(action.payload.formValue.price),
-                        category: action.payload.formValue.category,
-                        id: action.payload.id,
-                    }
-                ],
-            }
-            case "delete":
-                console.log(action.payload)
-                return {
-                    ...state,
-                    expense: state.expense.filter((el) => el.id !== action.payload)
-                }
-           
-        case 'total':
-          return {
-            ...state,
-            total: state.expense.reduce((acc, num) => acc + num.price, 0),
-          }
-        default:
-            return state;
-    }
-}
+    case "add":
+      return {
+        ...state,
+        expense: [
+          ...state.expense,
+          {
+            price: parseInt(action.payload.formValue.price),
+            category: action.payload.formValue.category,
+            id: action.payload.id,
+          },
+        ],
+      };
+    case "delete":
+      return {
+        ...state,
+        expense: state.expense.filter((el) => el.id !== action.payload),
+      };
 
-export default reducer
+    case "total":
+      return {
+        ...state,
+        total: state.expense.reduce((acc, num) => acc + num.price, 0),
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
