75703aca1029bd735743a54ab87cd8302b5f571b
Azones <59911700+Azonesbz@users.noreply.github.com>
2023-09-15 11:52:36 +0200

    added html structure to List

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const List = () => {
+  const [total, setTotal] = useState(0)
+  const expenses = [
+    {
+      price: 50,
+      category: 'Vêtements'
+    },
+    {
+      price: 580,
+      category: 'Impôt'
+    },
+    {
+      price: 86,
+      category: 'Essences'
+    }
+  ]
+
   return (
-    <div>List</div>
+    <div className='container'>
+      <ul className='list'>
+        <li className='row' id='head-row'>
+          <span>Prix</span>
+          <span>Catégorie</span>
+        </li>
+        <div style={{marginTop: '25px'}}>
+          {expenses.map((expense, index) => (<li key={index} className='row'>
+            <span>{expense.price}</span>
+            <span>{expense.category}</span>
+          </li>))}
+        </div>
+        <li className='row'>
+          <span>Total</span>
+          <span>{total}</span>
+        </li>
+      </ul>
+    </div>
   )
 }
 
